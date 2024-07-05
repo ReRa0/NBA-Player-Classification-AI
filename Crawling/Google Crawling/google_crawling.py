c7f214fb99f405ca2e5906dbc89021646df91e0b
adamkim <adamkimkh@gmail.com>
2024-07-05 23:33:38 +0900

    modified google crawling(final version)

--- Crawling/Google Crawling/google_crawling.py
+++ Crawling/Google Crawling/google_crawling.py
@@ -6,7 +6,6 @@ from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 
 import urllib.request
-import urllib.error
 import ssl
 import os
 import re
@@ -14,62 +13,52 @@ import time
 
 from nba_api.stats.static import players
 
-player_list = players.get_players()
-
+# Remove non-English characters from player names
 def remove_non_english(input_string):
     cleaned_string = re.sub(r'[^a-zA-Z]', '', input_string)
     return cleaned_string
 
-indexes_to_delete = []
-
-for i in range(len(player_list)):
-    if not player_list[i]['is_active']:
-        indexes_to_delete.append(i)
-indexes_to_delete.reverse()
-for i in indexes_to_delete:
+# Get active NBA players
+player_list = players.get_players()
+indexes_to_delete = [i for i in range(len(player_list)) if not player_list[i]['is_active']]
+for i in reversed(indexes_to_delete):
     del player_list[i]
+for player in player_list:
+    player['full_name'] = remove_non_english(player['full_name'])
 
-for i in range(len(player_list)):
-    player_list[i]['full_name'] = remove_non_english(player_list[i]['full_name'])
-
-
+# Setup SSL context
 ssl._create_default_https_context = ssl._create_unverified_context
 
+# Setup Chrome driver options
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
-
 chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
 service = Service(ChromeDriverManager().install())
 
+# Initialize Chrome driver
 driver = webdriver.Chrome(options=chrome_options)
 
-driver.get("https://www.google.com/")
-
+# Function to scroll down the page
 def scroll_down():
+    last_height = driver.execute_script("return document.body.scrollHeight")
     while True:
-        time.sleep(3)
-        # 페이지 맨 아래로 스크롤
         driver.find_element(By.XPATH, '//body').send_keys(Keys.END)
         time.sleep(1)
-        try:
-            # '더보기' 버튼이 보이면 클릭
-            load_more_button = driver.find_element(By.XPATH, '//*[@id="islmp"]/div/div/div/div/div[1]/div[2]/div[2]/input')
-            if load_more_button.is_displayed():
-                load_more_button.click()
-        except:
-            pass
-        time.sleep(1)
-        try:
-            # '더 이상 표시할 콘텐츠가 없습니다.' 메시지가 보이면 종료
-            no_more_content = driver.find_element(By.XPATH, '//div[@class="K25wae"]//*[text()="더 이상 표시할 콘텐츠가 없습니다."]')
-            if no_more_content.is_displayed():
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            try:
+                load_more_button = driver.find_element(By.XPATH, '//*[@id="islmp"]/div/div/div/div/div[1]/div[2]/div[2]/input')
+                if load_more_button.is_displayed():
+                    load_more_button.click()
+                    time.sleep(1)
+                else:
+                    break
+            except:
                 break
-        except:
-            pass
+        last_height = new_height
 
-
+# Crawl images for each player
 for i in range(len(player_list)):
-
     tag = player_list[i]['full_name']
     url = f'https://www.google.com/search?tbm=isch&q={tag}'
 
@@ -77,7 +66,6 @@ for i in range(len(player_list)):
     path = f'Crawling/Google Crawling/Google Player Image Data/{tag}'
 
     driver.get(url)
-
     driver.implicitly_wait(15)
 
     scroll_down()
@@ -86,8 +74,16 @@ for i in range(len(player_list)):
     
     for idx, image in enumerate(images):
         image_url = image.get_attribute('src')
+        if not image_url:
+            image_url = image.get_attribute('data-src')
+        if not image_url:
+            image_url = image.get_attribute('srcset').split(' ')[0] if image.get_attribute('srcset') else None
+        
         if image_url:
-            image_path = os.path.join(path, f"{tag}_{idx}.jpg")
-            urllib.request.urlretrieve(image_url, image_path)
+            try:
+                image_path = os.path.join(path, f"{tag}_{idx}.jpg")
+                urllib.request.urlretrieve(image_url, image_path)
+            except Exception as e:
+                print(f"Could not download {image_url}: {e}")
 
 driver.quit()
